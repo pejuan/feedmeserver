61c6ea6e5d490cce944d1a0cd18c3a87f5467a23
Xavier Munguia <xavier2696@gmail.com>
2016-02-20 18:38:01 -0600

    prueba servidor base de datos

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
  var morgan = require('morgan'); // log requests to the console (express4)
  var bodyParser = require('body-parser'); // pull information from HTML POST (express4)
  var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+ var pg = require('pg');
 
  app.use(bodyParser.json());
  app.use(express.static(__dirname + "/public"));
@@ -21,4 +22,20 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
+app.get('/clientes', function (request, response) {
+	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+		client.query('SELECT * FROM Cliente', function(err, result) {
+			done();
+			if (err){ 
+				console.error(err); response.send("Error " + err);
+				response.status(400).end(); 
+			}else{ 
+				//response.render('pages/db', {results: result.rows} ); 
+				response.contentType('application/json');
+                response.send(JSON.stringify(result));
+                response.status(200).end();
+			}
+		});
+	});
+});
 
