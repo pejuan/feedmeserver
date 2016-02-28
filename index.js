224fc5eab1520c33b0c60617fd7595e6aa310bf8
Xavier Munguia <xavier2696@gmail.com>
2016-02-28 09:56:32 -0600

    debug

--- index.js
+++ index.js
@@ -6,11 +6,8 @@
  var pg = require('pg');
  var cors = require('cors');
 
-var corsOptions = {
-  origin: 'https://feedmeclient.herokuapp.com/'
-};
 
- app.use(cors(corsOptions));
+ app.use(cors());
 
  app.use(bodyParser.json());
  app.use(express.static(__dirname + "/public"));
@@ -19,11 +16,6 @@ var corsOptions = {
 
 
 
- app.get('/', function(request, response) {
-     response.contentType('application/json');
-     response.send(JSON.stringify("{titulo:'prueba'}"));
-     response.status(200).end();
- });
 
  app.listen(app.get('port'), function() {
      console.log('Node app is running on port', app.get('port'));
