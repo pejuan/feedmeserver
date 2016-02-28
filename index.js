3fc8b528615f65055aea2ba2838d3c92ab8a1589
Xavier Munguia <xavier2696@gmail.com>
2016-02-28 11:01:32 -0600

    debug

--- index.js
+++ index.js
@@ -14,34 +14,7 @@
 
  app.set('port', (process.env.PORT || 5000));
 
-
-app.use(function(req, res, next) {
-    console.log("entra cors");
-    var oneof = false;
-    if(req.headers.origin) {
-        res.header('Access-Control-Allow-Origin', req.headers.origin);
-        oneof = true;
-    }
-    if(req.headers['access-control-request-method']) {
-        res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
-        oneof = true;
-    }
-    if(req.headers['access-control-request-headers']) {
-        res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
-        oneof = true;
-    }
-    if(oneof) {
-        res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365);
-    }
-
-    // intercept OPTIONS method
-    if (oneof && req.method == 'OPTIONS') {
-        console.log("entra options");
-        res.send(200).end();
-    }else {
-        next();
-    }
-});
+app.options('/order', cors());
 
  app.listen(app.get('port'), function() {
      console.log('Node app is running on port', app.get('port'));
