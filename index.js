30ae041e0750da30607ac3a9707eb8b83999491a
Kathia Barahona <katbara03@gmail.com>
2016-02-27 22:37:52 -0600

    create order

--- index.js
+++ index.js
@@ -7,56 +7,97 @@
  var cors = require('cors');
 
 
-app.use(cors());
+ app.use(cors());
 
  app.use(bodyParser.json());
  app.use(express.static(__dirname + "/public"));
 
-app.set('port', (process.env.PORT || 5000));
+ app.set('port', (process.env.PORT || 5000));
 
 
 
-app.get('/', function(request, response) {
-	response.contentType('application/json');
-    response.send(JSON.stringify("{titulo:'prueba'}"));
-    response.status(200).end();
-});
+ app.get('/', function(request, response) {
+     response.contentType('application/json');
+     response.send(JSON.stringify("{titulo:'prueba'}"));
+     response.status(200).end();
+ });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+ app.listen(app.get('port'), function() {
+     console.log('Node app is running on port', app.get('port'));
+ });
 
-app.get('/clientes', function (request, response) {
-	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-		client.query('SELECT * FROM Cliente', function(err, result) {
-			done();
-			if (err){ 
-				console.error(err); response.send("Error " + err);
-				response.status(400).end(); 
-			}else{ 
-				//response.render('pages/db', {results: result.rows} ); 
-				response.contentType('application/json');
-                response.send(JSON.stringify(result.rows));
-                response.status(200).end();
-			}
-		});
-	});
-});
+ app.get('/clientes', function(request, response) {
+     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+         client.query('SELECT * FROM Cliente', function(err, result) {
+             done();
+             if (err) {
+                 console.error(err);
+                 response.send("Error " + err);
+                 response.status(400).end();
+             } else {
+                 //response.render('pages/db', {results: result.rows} ); 
+                 response.contentType('application/json');
+                 response.send(JSON.stringify(result.rows));
+                 response.status(200).end();
+             }
+         });
+     });
+ });
 
-app.get('/comidas', function (request, response) {
-	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-		client.query('SELECT C.id_comida,C.nombre,C.precio,C.descripcion,C.categoria,C.foto,C.veces_ordenada,C.id_restaurante,R.nom_restaurante FROM Comida C join Restaurante R on C.id_restaurante=R.id_usuario', function(err, result) {
-			done();
-			if (err){ 
-				console.error(err); response.send("Error " + err);
-				response.status(400).end(); 
-			}else{ 
-				//response.render('pages/db', {results: result.rows} ); 
-				response.contentType('application/json');
-                response.send(JSON.stringify(result.rows));
-                response.status(200).end();
-			}
-		});
-	});
-});
+ app.get('/comidas', function(request, response) {
+     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+         client.query('SELECT C.id_comida,C.nombre,C.precio,C.descripcion,C.categoria,C.foto,C.veces_ordenada,C.id_restaurante,R.nom_restaurante FROM Comida C join Restaurante R on C.id_restaurante=R.id_usuario', function(err, result) {
+             done();
+             if (err) {
+                 console.error(err);
+                 response.send("Error " + err);
+                 response.status(400).end();
+             } else {
+                 //response.render('pages/db', {results: result.rows} ); 
+                 response.contentType('application/json');
+                 response.send(JSON.stringify(result.rows));
+                 response.status(200).end();
+             }
+         });
+     });
+ });
+ app.post('/order', function(request, response) {
+  
+     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+         var sql = { query: 'INSERT INTO ', table: 'Orden', columns: ['id_cliente', 'id_restaurante'] }
 
+         sql.values = [11341025, request.idRestaurant];
+
+         client.query(sql.query + sql.table + " (" + sql.columns.join(',') + ") " + "VALUES (" + sql.values.join(',') + ")", function(err, result) {
+             done();
+             if (err) {
+                 console.error(err);
+                 response.send("Error " + err);
+                 response.status(400).end();
+             } else {
+                 //response.render('pages/db', {results: result.rows} ); 
+                 for (var j = 0; j < request.foods; j++) {
+                     sql.values = [response.id_orden, request.foods[j].id_comida];
+                     sql.columns = ["id_orden", "id_comida"];
+                     sql.table = "Comida_pertenece_orden";
+                     client.query(sql.query + sql.table + " (" + sql.columns.join(',') + ") " + "VALUES (" + sql.values.join(',') + ")", function(err, result) {
+                         done();
+                         if (err) {
+                             console.error(err);
+                             response.send("Error " + err);
+                             response.status(400).end();
+                         } else {
+                             //response.render('pages/db', {results: result.rows} ); 
+                             response.contentType('application/json');
+                             response.send(JSON.stringify(result.rows));
+                             response.status(200).end();
+                         }
+                     });
+                 }
+             }
+
+         });
+
+
+     });
+ });
