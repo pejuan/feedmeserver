866a8790f002c491603322e868b7dfb7f2071d88
Xavier Munguia <xavier2696@gmail.com>
2016-02-28 08:46:11 -0600

    debug

--- index.js
+++ index.js
@@ -64,10 +64,9 @@
  app.post('/order', function(request, response) {
 
      pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-        console.log(request);
          var sql = { query: 'INSERT INTO ', table: 'Orden', columns: ['id_cliente', 'id_restaurante','estado'] };
 
-         sql.values = ['11341025', "\'"+request.idRestaurant+"\'","\'N\'"];
+         sql.values = ['11341025', "\'"+request.body.idRestaurant+"\'","\'N\'"];
    		
          client.query(sql.query + sql.table + " (" + sql.columns.join(',') + ") " + "VALUES (" + sql.values.join(',') + ")", function(err, result) {
              done();
