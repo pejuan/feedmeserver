5d49d8be627259e5ee97915b587f17a14936b647
Xavier Munguia <xavier2696@gmail.com>
2016-02-28 10:52:55 -0600

    debug

--- index.js
+++ index.js
@@ -15,16 +15,33 @@
  app.set('port', (process.env.PORT || 5000));
 
 
- app.options('/order', function (request, response) {
-  console.log("entra options");
-  //console.log(request);
-    response.contentType('application/json');
-    response.send(JSON.stringify(result.rows));
-    
-    response.status(201).end();
-  //next(); // pass control to the next handler
-});
+app.use(function(req, res, next) {
+    console.log("entra cors");
+    var oneof = false;
+    if(req.headers.origin) {
+        res.header('Access-Control-Allow-Origin', req.headers.origin);
+        oneof = true;
+    }
+    if(req.headers['access-control-request-method']) {
+        res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
+        oneof = true;
+    }
+    if(req.headers['access-control-request-headers']) {
+        res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
+        oneof = true;
+    }
+    if(oneof) {
+        res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365);
+    }
 
+    // intercept OPTIONS method
+    if (oneof && req.method == 'OPTIONS') {
+        res.send(200);
+    }
+    else {
+        next();
+    }
+});
 
  app.listen(app.get('port'), function() {
      console.log('Node app is running on port', app.get('port'));
