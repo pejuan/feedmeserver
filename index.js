124403f088430c73bfab3f6bd3f622e5a315ad91
Xavier Munguia <xavier2696@gmail.com>
2016-02-27 20:54:47 -0600

    comida retorna restaurante

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.get('/clientes', function (request, response) {
 
 app.get('/comidas', function (request, response) {
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-		client.query('SELECT C.id_comida,R.nom_restaurante FROM Comida C join Restaurante R where C.id_restaurante=R.id_usuario', function(err, result) {
+		client.query('SELECT C.id_comida,R.nom_restaurante FROM Comida C join Restaurante R on C.id_restaurante=R.id_usuario', function(err, result) {
 			done();
 			if (err){ 
 				console.error(err); response.send("Error " + err);
