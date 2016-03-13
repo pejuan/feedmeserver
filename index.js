822d04af95e08c5e8d259ea0256adbd052ff6260
Kathia Barahona <katbara03@gmail.com>
2016-03-12 22:28:58 -0600

    precio comida

--- index.js
+++ index.js
@@ -218,26 +218,16 @@ app.options('/order', cors());
      });
  });
 
-//var client2= new pg.Client(process.env.DATABASE_URL);
+var client2= new pg.Client(process.env.DATABASE_URL);
 app.post('/historialOrdenes', function(req, res) {
     //var client ;
-    var client= new pg.Client(process.env.DATABASE_URL)
      //pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-         client.connect(function(err) {
+         client2.connect(function(err) {
         var sql = { query: 'SELECT R.nom_restaurante, O.id_orden, O.id_restaurante, O.estado, O.ispaid, O.tiempo, O.id_cliente FROM ', table: 'Orden O', join:' inner join Restaurante R on R.id_usuario=O.id_restaurante' , where: ' where O.id_cliente = '+"\'"+req.body.id_cliente+"\'"};
-          var query = client.query(sql.query + sql.table + sql.join+sql.where, function(err, projects) {
+          client2.query(sql.query + sql.table + sql.join+sql.where, function(err, projects) {
             //console.log(sql.query + sql.table + sql.join+sql.where);
         if (err) return console.error("error1"+err);
-        async.each(projects.rows,
-            function(projectRow, cb) { // called once for each project row
-                client.query('select CO.id,CO.id_orden,CO.id_comida,C.nombre from Comida_pertenece_orden CO inner join Comida C on C.id_comida = CO.id_comida where CO.id_orden= '+"\'"+projectRow.id_orden+"\'" , function(err, result) {
-                  if(err) return cb("erro3"+err); // let Async know there was an error. Further processing will stop
-                  projectRow.comidas = result.rows;
-                  cb(null); // no error, continue with next projectRow, if any
-                });
-              }
-
-        , function(err) {
+        async.each(projects.rows, addComidasToOrden, function(err) {
           if (err) return console.error("error2"+err);
           // all project rows have been handled now
           //console.log(projects.rows);
@@ -246,14 +236,14 @@ app.post('/historialOrdenes', function(req, res) {
           res.status(200).end();
         });
       });
-    //query.on('end', function() { client.end(); });
-    });
+    //});
     //client.end();
-     //});
- });
+     });
+ });    
 
 var addComidasToOrden = function(projectRow, cb) { // called once for each project row
-    client2.query('select CO.id,CO.id_orden,CO.id_comida,C.nombre from Comida_pertenece_orden CO inner join Comida C on C.id_comida = CO.id_comida where CO.id_orden= '+"\'"+projectRow.id_orden+"\'" , function(err, result) {
+    client2.query('select CO.id,CO.id_orden,CO.id_comida,C.nombre, C.precio from Comida_pertenece_orden CO inner join Comida C on C.id_comida = CO.id_comida where CO.id_orden= '+"\'"+projectRow.id_orden+"\'" , function(err, result) {
+        //console.log('select CO.id,CO.id_orden,CO.id_comida,C.nombre from Comida_pertenece_orden CO join Comida C on C.id_comida = CO.id_comida where CO.id_orden= '+"\'"+projectRow.id_orden+"\'");
       if(err) return cb("erro3"+err); // let Async know there was an error. Further processing will stop
       projectRow.comidas = result.rows;
       cb(null); // no error, continue with next projectRow, if any
