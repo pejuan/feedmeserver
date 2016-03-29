9643d08af3c9c3e5909b18680202458c6ebd899f
JoedHernandez1995 <joedhernandez95@unitec.edu>
2016-03-28 23:05:33 -0600

    probando el login

--- index.js
+++ index.js
@@ -137,7 +137,7 @@ app.post('/loginRestaurante', function(req, res) {
 
                 }else if(result.rows.length > 0){
                     res.header("Access-Control-Allow-Origin: http://localhost:8100");
-                    res.send("existe");
+                    
                     res.status(200).end();
 
                 } else {
@@ -250,7 +250,7 @@ app.get('/restaurantes',function(request, response) {
                          response.contentType('application/json');
                          response.status(201).end();
                          console.log("Done");
-        
+
                      }
                  });
                  response.contentType('application/json');
