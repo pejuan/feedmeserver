859d4b5688b2109da3baeed284988eed09c9115c
Xavier Munguia <xavier2696@gmail.com>
2016-06-04 19:08:38 -0600

    endpoints comentarios

--- index.js
+++ index.js
@@ -988,3 +988,44 @@ app.post('/noMeGusta', function(request, response) {
          });
      });
  });
+
+ app.post('/comentario', function(request, response) {
+    pg.connect(process.env.DATABASE_URL,function(err, client, done) {        
+        client.query("insert into Comentario(id,id_comida,comentario,id_cliente) values(DEFAULT,"+"\'"+request.body.id_comida+"\',"+"\'"+request.body.comentario+"\',"+"\'"+request.body.id_cliente+"\'"+")",function(err,result){
+            console.log("insert into Comentario(id,id_comida,comentario,id_cliente) values(DEFAULT,"+"\'"+request.body.id_comida+"\',"+"\'"+request.body.comentario+"\',"+"\'"+request.body.id_cliente+"\'"+")");
+             done();
+             if (err) {
+                 console.log(err);
+                 response.send(err);
+                 response.status(400).end();
+             }else{
+                //response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+                 //response.render('pages/db', {results: result.rows};hero
+                 response.contentType('application/json');
+                 response.status(201).end();
+                 console.log("Done");
+
+             }
+        });
+    });
+});
+
+app.post('/comentariosComida', function(request, response) {
+     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+         client.query('SELECT * FROM Comentario where id_comida='+"\'"+request.body.id_comida+"\'", function(err, result) {
+             done();
+             if (err) {
+                 console.error(err);
+                 response.header("Access-Control-Allow-Origin: http://localhost:8100");
+                 response.send("Error " + err);
+                 response.status(400).end();
+             } else {
+                 //response.render('pages/db', {results: result.rows} );
+                 response.header("Access-Control-Allow-Origin: http://localhost:8100");
+                 response.contentType('application/json');
+                 response.send(JSON.stringify(result.rows));
+                 response.status(200).end();
+             }
+         });
+     });
+ });
